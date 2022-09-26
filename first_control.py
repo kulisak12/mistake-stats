1e7491c2704fa164a7300cc74a5d04128b9e68f5
kulisak12 <kulisak12lp@gmail.com>
2022-09-26 09:33:57 +0200

    Adjust first_control to the new system

--- first_control.py
+++ first_control.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import datetime
-from typing import List
+from typing import List, Tuple
 from course import Course, SMALL_MISTAKE_THRESHOLD, BIG_MISTAKE_THRESHOLD
 from datasets import DK_ORIS_ID, load_user_races_dataset
 
@@ -12,35 +12,35 @@ def main():
     )
     for course in courses:
         course.set_adjustments()
-        course.set_relative_splits()
-        course.calculate_z_scores()
+        course.calculate_losses()
 
     max_controls = max(course.num_controls for course in courses)
-    mistake_splits: List[int] = []
-    total_splits: List[int] = []
+    mistake_percentages: List[float] = []
     for control in range(max_controls):
-        mistakes = sum(
-            count_mistakes_for_control(course, control, SMALL_MISTAKE_THRESHOLD)
-            for course in courses
-        )
-        total = sum(
-            len(course.competitors)
-            for course in courses
-            if control < course.num_controls
-        )
-        mistake_splits.append(mistakes)
-        total_splits.append(total)
+        mistakes_sum = 0
+        total_sum = 0
+        for course in courses:
+            mistakes, total = count_mistakes_for_control(
+                course, control, SMALL_MISTAKE_THRESHOLD
+            )
+            mistakes_sum += mistakes
+            total_sum += total
+        mistake_percentages.append(mistakes_sum / total_sum)
 
-    mistake_percentage = [mistake / total for mistake, total in zip(mistake_splits, total_splits)]
-    print(mistake_percentage)
+    print(mistake_percentages)
 
 
-def count_mistakes_for_control(course: Course, control: int, threshold: float) -> int:
+def count_mistakes_for_control(course: Course, control: int, threshold: float) -> Tuple[int, int]:
+    """Return number of mistakes and total number of considered splits."""
+
     num_mistakes = 0
+    num_splits = 0
     for c in course.competitors:
-        if len(c.z_scores) > control and c.z_scores[control] > threshold:
-            num_mistakes += 1
-    return num_mistakes
+        if control < len(c.losses):
+            num_splits += 1
+            if c.made_mistake(control, threshold):
+                num_mistakes += 1
+    return num_mistakes, num_splits
 
 
 if __name__ == '__main__':
