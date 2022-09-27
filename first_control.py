8f4c3febc41858ee0fd6aaa0effb2cde2d6f5e71
kulisak12 <kulisak12lp@gmail.com>
2022-09-27 13:20:44 +0200

    Calculate p-value

--- first_control.py
+++ first_control.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 import datetime
-from typing import List
+import math
 from course import SMALL_MISTAKE_THRESHOLD, BIG_MISTAKE_THRESHOLD
 from datasets import DK_ORIS_ID, DK_NAME, load_user_races_dataset
+from scipy.stats import norm
+from typing import List
 
 
 def main():
@@ -13,7 +15,6 @@ def main():
     for course in courses:
         course.set_adjustments()
         course.calculate_losses()
-    mistake_percentages: List[float] = []
 
     competitors = [
         c
@@ -23,17 +24,35 @@ def main():
     competitors = list(filter(lambda x: x.name == DK_NAME, competitors))
 
     max_controls = max(len(c.losses) for c in competitors)
+    num_mistakes = [0 for _ in range(max_controls)]
+    num_splits = [0 for _ in range(max_controls)]
     for control in range(max_controls):
-        mistakes = 0
-        total_splits = 0
         for c in competitors:
             if control < len(c.losses):
-                total_splits += 1
+                num_splits[control] += 1
                 if c.made_mistake(control, SMALL_MISTAKE_THRESHOLD):
-                    mistakes += 1
-        mistake_percentages.append(mistakes / total_splits)
+                    num_mistakes[control] += 1
 
-    print(mistake_percentages)
+    # print mistake percentages
+    print([m / s for m, s in zip(num_mistakes, num_splits)])
+
+    first_control_mistakes = num_mistakes[0]
+    first_control_splits = num_splits[0]
+    first_control_p = first_control_mistakes / first_control_splits
+    print(f"{first_control_p=}")
+    other_control_mistakes = sum(num_mistakes[1:])
+    other_control_splits = sum(num_splits[1:])
+    other_control_p = other_control_mistakes / other_control_splits
+    print(f"{other_control_p=}")
+
+    # null hypothesis: mistake probability is always the same
+    mistake_probability = sum(num_mistakes) / sum(num_splits)
+    var = mistake_probability * (1 - mistake_probability)
+    # test statistic is the difference of means
+    difference = first_control_p - other_control_p
+    difference_var = var * (1 / first_control_splits + 1 / other_control_splits)
+    p_value = norm.cdf(difference, scale=math.sqrt(difference_var))
+    print(f"{p_value=}")
 
 
 if __name__ == '__main__':
